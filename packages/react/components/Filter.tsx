c31f777dc3faa471679a4efbe5bb5f510cb154f3
xuhui <xuhui_er@outlook.com>
2024-01-29 23:11:05 +0800

    enhance the `Filter` component

--- packages/react/components/Filter.tsx
+++ packages/react/components/Filter.tsx
@@ -1,25 +1,53 @@
-import { ReactNode, useEffect, useRef } from 'react';
-import { Control, FieldValues, Path, UseFormProps, useForm } from 'react-hook-form';
+import { ReactNode, useEffect, useRef, useState } from 'react';
+import {
+  Control,
+  Controller,
+  ControllerProps,
+  FieldValues,
+  Path,
+  PathValue,
+  UseFormProps,
+  useForm
+} from 'react-hook-form';
 import { changeQueryRequester, queryChangedNotifier } from '../plugins/SearchChangePlugin';
 import { mergeRefs } from '../utils/mergeRefs';
 import { omit } from 'lodash-es';
 
-interface FilterProps<T extends FieldValues>
+export interface ItemProps<T extends FieldValues> {
+  fieldId: Path<T>;
+  /**
+   * @default ""
+   */
+  defaultValue?: PathValue<T, Path<T>>;
+  /**
+   * Don't use with `label` together.
+   */
+  render?: ControllerProps<T, Path<T>>['render'];
+}
+
+export interface FilterProps<T extends FieldValues>
   extends Omit<React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement>, 'onSubmit'> {
   ref?: ((instance: HTMLFormElement | null) => void) | React.RefObject<HTMLFormElement>;
   useFormProps?: UseFormProps<T, any>;
-  formRender?: (control: Control<T, Path<T>>) => ReactNode;
+  items: ItemProps<T>[];
+  /**
+   * @default 4
+   */
+  limit?: number;
   submitButton?: ReactNode;
   resetButton?: ReactNode;
+  expandButton?: ReactNode;
+  collapseButton?: ReactNode;
+  buttonGroupClassName?: string;
 }
 
 /**
  * The component is built on react-hook-form. Something notable:
- * 
+ *
  * 1. The component depends on SearchParamsPlugin.
- * 
+ *
  * 2. All fields will be set to '' when reset.
- * 
+ *
  * 3. Every field needs a default value.
  * @param props
  * @returns
@@ -29,6 +57,17 @@ export function Filter<T extends FieldValues>(props: FilterProps<T>) {
   const formProps = omit(props, ['useFormProps', 'formRender', 'submitButton', 'resetButton']);
   const _formRef = useRef<HTMLFormElement>(null);
   const formRef = props.ref ? mergeRefs(props.ref, _formRef) : _formRef;
+  const [itemsExpanded, setItemsExpanded] = useState(false);
+  const limit = props.limit ?? 4;
+  const items = itemsExpanded ? props.items : props.items.slice(0, limit);
+
+  const ExpandOrCollapseButton = () => {
+    if (itemsExpanded) {
+      return <span onClick={() => setItemsExpanded(false)}>{props.collapseButton ?? 'collapse'}</span>;
+    } else {
+      return <span onClick={() => setItemsExpanded(true)}>{props.expandButton ?? 'expand'}</span>;
+    }
+  };
 
   useEffect(() => {
     return queryChangedNotifier.receive(async ({ payload }) => {
@@ -56,15 +95,35 @@ export function Filter<T extends FieldValues>(props: FilterProps<T>) {
       ref={formRef}
       {...formProps}
       onSubmit={handleSubmit(data => {
-        console.log(data);
         changeQueryRequester.send({ query: data, force: true, mode: 'merge' });
       })}
     >
-      {props.formRender?.(control)}
+      {items.map(({ fieldId, render, defaultValue }) => (
+        <Controller<T, Path<T>>
+          key={fieldId}
+          name={fieldId}
+          control={control}
+          // @ts-expect-error
+          defaultValue={defaultValue ?? ''}
+          render={
+            render ??
+            (({ field }) => (
+              <label>
+                <span>{fieldId}</span>
+                <input {...field} />
+              </label>
+            ))
+          }
+        />
+      ))}
 
-      {props.submitButton ? <span onClick={filter}>{props.submitButton}</span> : null}
+      <span className={props.buttonGroupClassName}>
+        {props.submitButton ? <span onClick={filter}>{props.submitButton}</span> : 'submit'}
 
-      {props.resetButton ? <span onClick={resetAndFilter}>{props.resetButton}</span> : null}
+        {props.resetButton ? <span onClick={resetAndFilter}>{props.resetButton}</span> : 'reset'}
+
+        {props.items.length > limit && <ExpandOrCollapseButton />}
+      </span>
     </form>
   );
 }
